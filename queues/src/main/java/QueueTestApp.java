8b41526f85269e7159d09b14146fe16fc867adea
BetulB <betul.bogrek@gmail.com>
2022-03-19 09:36:49 -0500

    updates

---
+++ queues/src/main/java/QueueTestApp.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class QueueTestApp {
+    public static void main(String[] args) {
+        MyQueue<Integer> myQueue = new MyQueue<>();
+        System.out.println(myQueue.isEmpty());
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        System.out.println(myQueue.peek());
+        System.out.println("removed item from Queue is: "+ myQueue.dequeue());
+        HashMap<Integer, String> a=new HashMap<>();
+        a.put(1, "Sergui");
+        System.out.println(a.containsValue("Sergui"));
+    }
+}
